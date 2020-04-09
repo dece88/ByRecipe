192904453ef60bb3149b702a2d8f3953a93b60b7
Daniel Christianto <daniel.christianto3@gmail.com>
2020-04-10 01:16:14 +0700

    Add change picture button

--- app/src/main/java/com/example/byrecipe/Activity/UserProfileActivity.kt
+++ app/src/main/java/com/example/byrecipe/Activity/UserProfileActivity.kt
@@ -1,21 +1,33 @@
 package com.example.byrecipe.Activity
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.webkit.ValueCallback
 import android.widget.Button
+import android.widget.ImageView
 import com.example.byrecipe.DBHelper.DBHelperUser
 import com.example.byrecipe.Model.User
 import com.example.byrecipe.R
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.File
 
 class UserProfileActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var user: User
     private lateinit var dbUser: DBHelperUser
+    lateinit var imageProfile: Uri
 
     companion object{
         const val USER = "session user"
+        const val PICK_IMAGE: Int = 100
+        const val UPDATE_DATA: Int = 200
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +37,8 @@ class UserProfileActivity : AppCompatActivity(), View.OnClickListener {
         setUserProfile() //function to get and set session user
 
         //Set of button
+        val btnChangePicture: Button = findViewById(R.id.user_profile_button_changePicture)
+        btnChangePicture.setOnClickListener(this)
         val btnEditProfile: Button = findViewById(R.id.user_profile_button_editProfile)
         btnEditProfile.setOnClickListener(this)
     }
@@ -48,6 +62,25 @@ class UserProfileActivity : AppCompatActivity(), View.OnClickListener {
                 moveToEditProfile.putExtra(EditUserProfileActivity.USER, user)
                 startActivity(moveToEditProfile)
             }
+
+            R.id.user_profile_button_changePicture -> {
+                val gallery: Intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(gallery, PICK_IMAGE)
+            }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if((resultCode == Activity.RESULT_OK) and (requestCode==PICK_IMAGE)){
+            imageProfile = data?.getData()!!
+            val imageView: ImageView = findViewById(R.id.profile_image)
+            imageView.setImageURI(imageProfile)
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Alert!")
+            builder.setMessage("Picture is broken!")
+        }
+    }
+
 }
