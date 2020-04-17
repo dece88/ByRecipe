8adb5f021e90113c160ef7003b73664ad5ab6e81
Daniel Christianto <daniel.christianto3@gmail.com>
2020-04-18 00:41:15 +0700

    Add Contact

---
+++ app/src/main/java/com/example/byrecipe/Activity/ContactActivity.kt
@@ -0,0 +1,13 @@
+package com.example.byrecipe.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.byrecipe.R
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
